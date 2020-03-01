5d3f819c98186bc98a86ada17b039fe01cf11e73
Marco Cosentino <cosentino.ma@gmail.com>
2020-03-01 07:07:57 +0100

    Always log to console

--- server/src/util/logger.ts
+++ server/src/util/logger.ts
@@ -12,20 +12,12 @@ const logger = createLogger({
     ),
     transports: [
         new transports.File({ filename: 'econfig-combined.log' }),
+        new transports.Console({
+            format: format.combine(
+                format.colorize(),
+                format.simple()),
+            }),
     ],
 });
-
-//
-// If we're not in production then **ALSO** log to the `console`
-// with the colorized simple format.
-//
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new transports.Console({
-        format: format.combine(
-            format.colorize(),
-            format.simple(),
-        ),
-    }));
-}
 
 export default logger;
